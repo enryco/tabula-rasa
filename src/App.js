84e6b1386158d445be8baa48a2ef8c383f13b0f4
Enrico Scherlies <e.scherlies@me.com>
2018-05-12 11:49:27 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Done 🎉
+      </div>
+    );
+  }
+}
+
+export default App;
+
